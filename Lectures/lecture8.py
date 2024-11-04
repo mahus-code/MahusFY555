135e4ab239b654bd8ad956e4d850dae15f7f2c28
mahus-code <mangoshusted@gmail.com>
2024-11-04 12:26:52 +0100

    lecture8 + data added

---
+++ Lectures/lecture8.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pickle
+
+# If you don't have data yyet:
+z = [0, 0.01, 0.02]
+dL = [10, 20, 30]
+
+with open(r"C:\Users\magnu\VsProjects\fy555_python\MahusFY555\Lectures\z.txt", "rb") as fp:
+    z = pickle.load(fp)
+with open(r"C:\Users\magnu\VsProjects\fy555_python\MahusFY555\Lectures\dL.txt", "rb") as fp:
+    dL = pickle.load(fp)
+
+z = z.reshape(-1,1)
+
+from sklearn.linear_model import SGDRegressor
+
+sgd_regressor = SGDRegressor(max_iter = 1000, eta0 = 0.1, random_state = 42)
+
+lin_r = sgd_regressor.fit(z, dL)
+ypred = lin_r.predict(z)
+print(lin_r.coef_)
+print(lin_r.intercept_)
+
+plt.plot(z,dL,'*')
+plt.plot(z, ypred, '-')
+
+from sklearn.preprocessing import PolynomialFeatures
+
+quadratic = PolynomialFeatures(degree=2)
+z_quad = quadratic.fit_transform(z)
+#print(z_quad)
+
+poly_r = sgd_regressor.fit(z_quad, dL)
+dL_quad = poly_r.predict(z_quad)
+plt.plot(z,dL_quad,'--')
+plt.show()

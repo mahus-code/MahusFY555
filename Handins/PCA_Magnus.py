ec46eee49efa92e9c5458004587891949ece8c0e
mahus-code <mangoshusted@gmail.com>
2024-12-04 19:00:19 +0100

    PCA

---
+++ Handins/PCA_Magnus.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from astroML.datasets import sdss_corrected_spectra
+
+# Fetch the data
+data = sdss_corrected_spectra.fetch_sdss_corrected_spectra()
+spectra = sdss_corrected_spectra.reconstruct_spectra(data)
+lam = sdss_corrected_spectra.compute_wavelengths(data)
+print(np.unique(data))
+
+# First we normalize the spectra (row wise)
+normalized_spectra = preprocessing.normalize(spectra, axis=1)
+
+# Calculate the mean and standard deviation across all spectra
+mean_flux = normalized_spectra.mean(axis=0)
+std_flux = normalized_spectra.std(axis=0)
+
+# Utilize the mean spectrum code from the example
+wavelengths = lam
+std = spectra.std(0)
+plt.plot(lam, mean_flux, color='black')
+plt.fill_between(wavelengths, mean_flux - std_flux, mean_flux + std_flux, color='#CCCCCC')
+plt.xlim(wavelengths[0], wavelengths[-1]) # Gives last element
+plt.ylim(0, 0.06)
+plt.xlabel('wavelength (Angstroms)')
+plt.ylabel('scaled flux')
+plt.title('Mean Spectrum')
+plt.grid(True)
+plt.show()
+
+X = normalized_spectra
+from sklearn.decomposition import PCA
+
+# Perform PCA
+rpca = PCA(n_components=4, svd_solver='randomized', random_state=0)
+X_proj = rpca.fit_transform(X)
+print(X_proj.shape)
+y = data['z'] # where z is the redshift
+
+plt.figure()
+scatter =plt.scatter(X_proj[:, 0], X_proj[:, 1], c=y, s=4, lw=0, vmin=y.min(), vmax=y.max(), cmap=plt.cm.jet)
+plt.colorbar(scatter, label="Redshift (z)")
+plt.xlabel('coefficient 1')
+plt.ylabel('coefficient 2')
+plt.title('PCA projection of Spectra')
+plt.grid(True)
+plt.show()
+
+plt.figure()
+l = plt.plot(wavelengths, rpca.mean_ - 0.15)
+c = l[0].get_color()
+plt.text(7000, -0.16, "mean", color=c)
+for i in range(4):
+    l = plt.plot(wavelengths, rpca.components_[i] + 0.15 * i)
+    c = l[0].get_color()
+    plt.text(7000, -0.01 + 0.15 * i, "component %i" % (i + 1), color=c)
+plt.ylim(-0.2, 0.6)
+plt.xlabel('wavelength (Angstroms)')
+plt.ylabel('scaled flux + offset')
+plt.title('Mean Spectrum and Eigen-spectra')
+plt.grid(True)
+plt.show()
+
+print("This script loads SDSS data from astroML, normalizes the spectra, and plots the mean spectrum.") 
+print("A PCA analysis is performed to reduce the dimensionality of the data to four components.")
+print("The first scatter plot of the second component versus the first component illustrates how the spectra vary along these two axes, which capture the most variance and thereby provide the most information about the data.")
+print("The color-coding of the plot shows how redshift correlates with these two components.")
+print("The final plot shows the four principal components (eigen-spectra). The first component accounts for the largest variance in the spectra, with upward peaks likely being emission lines and downward peaks; absorption lines.") 
+print("The subsequent components capture progressively smaller contributions to the variance in the dataset.")
---
+++ Handins/feature_importance_Magnus.py
@@ -0,0 +1,72 @@
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+wine = datasets.load_wine()
+x = wine.data
+y = wine.target
+
+# Random forest:
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+
+forest = RandomForestClassifier(n_estimators=100, random_state=10)
+forest.fit(x,y)
+
+# Feature importance:
+importance = forest.feature_importances_
+
+# Sort in decending order for plot:
+indices = np.argsort(importance)[::-1]
+feat_labels = wine.feature_names
+# print(feat_labels)
+final_labels = []
+feat_labels = ['alcohol', 'malic acid', 'ash', 'alcalinity', 'magnesium', 'total phenols', 'flavanoids', 'nonflavanoid phenols', 'proanthocyanins', 'color intensity', 'hue', 'od280/od315', 'proline']
+
+for i in indices:
+    final_labels.append(feat_labels[i])
+
+fig, ax = plt.subplots(1,1)
+
+plt.title('Feature importane of wine data set')
+ax.bar(range(x.shape[1]), importance[indices], align = 'center', color='indianred')
+ax.set_xticks(range(x.shape[1]))
+ax.set_xticklabels(final_labels, fontsize='small', rotation = 35)
+ax.set_ylabel('Importance')
+plt.show()
+
+''' Comparing important feature selection '''
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+# Extract only the most important feature
+x_oneFeat = x[:, wine.feature_names.index('color_intensity')].reshape(-1,1)
+
+xtrain_oneFeat, xtest_oneFeat, ytrain_oneFeat, ytest_oneFeat = train_test_split(x_oneFeat, y, test_size=0.3, random_state=1, stratify=y)
+xtrain_all, xtest_all, ytrain_all, ytest_all = train_test_split(x, y, test_size=0.3, random_state=1, stratify=y)
+
+# Preprocessing the data
+
+scOneFeat = StandardScaler()
+scOneFeat.fit(xtrain_oneFeat)
+xtrain_oneFeat_std = scOneFeat.transform(xtrain_oneFeat)
+xtest_oneFeat_std = scOneFeat.transform(xtest_oneFeat)
+
+scAllFeat = StandardScaler()
+scAllFeat.fit(xtrain_all)
+xtrain_allFeat_std = scAllFeat.transform(xtrain_all)
+xtest_allFeat_std = scAllFeat.transform(xtest_all)
+
+ppn_one = Perceptron(max_iter=100, alpha=0.05, random_state=1)
+ppn_all = Perceptron(max_iter=100, alpha=0.05, random_state=1)
+
+ppn_one.fit(xtrain_oneFeat_std, ytrain_oneFeat)
+ppn_all.fit(xtrain_allFeat_std, ytrain_all)
+
+ypred_one = ppn_one.predict(xtest_oneFeat_std)
+ypred_all = ppn_all.predict(xtest_allFeat_std)
+
+print('Accuracy of model trained with most important feature (color intensity):', accuracy_score(ytest_oneFeat, ypred_one))
+print('Accuracy of model trained with all features:', accuracy_score(ytest_all, ypred_all))
+
